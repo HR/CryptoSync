05d09c12f56715ecc716fa5060a94955756e4596
Habib R <Habib.R@outlook.com>
2015-11-07 13:14:44 +0000

    start auth

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,7 @@
-
+var passport = require('passport');
 // Environment variables
 // Google: process.env.CS_GAPI_CID, process.env.CS_GAPI_S
 // Dropbox: process.env.CS_DBAPI_KEY, process.env.CS_DBAPI_SKEY
- 
+/* TO DO:
+* - Add auth
+*/
